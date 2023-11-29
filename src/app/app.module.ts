2a365adfc4225b3baced78168b9c2e7ca13289a6
tmampa <tmampa94@gmail.com>
2023-11-29 14:16:19 +0200

    Carousel implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
+import { CarouselComponent } from './_components/carousel/carousel.component';
+import { HomeComponent } from './_pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CarouselComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
